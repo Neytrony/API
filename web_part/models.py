40dcba13303767cb2276c61e261c7521ba2f4ba6
Alexander <sasha456852d@gmail.com>
2023-07-24 14:57:44 +0300

    fix: change model name

--- web_part/models.py
+++ web_part/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 # Create your models here.
-class Files(models.Model):
+class File(models.Model):
     name = models.CharField(max_length=255, null=True, blank=True)
     fileField = models.FileField(upload_to='', null=True, blank=True, verbose_name="Файл", storage=FileSystemStorage)
     task_id = models.CharField(max_length=255, null=True, blank=True)
